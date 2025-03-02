14a70470bd063736fb216d7e1d1edfaddc49a5c2
Ivan <vany4golos@yandex.ru>
2025-03-02 15:35:08 +0100

    f7

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './components/App.tsx'
 
-const basename = "/flexit-token";
+const basename = "/";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
