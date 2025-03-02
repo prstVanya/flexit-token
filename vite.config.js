df2e2372c181ee358b80d46c6e6086e37d6918dd
Ivan <vany4golos@yandex.ru>
2025-03-02 09:43:35 +0100

    fix: paths

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import mkcert from 'vite-plugin-mkcert';
 import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
+    base: "/",
     plugins: [
         react(),
         tsconfigPaths(),
